4623f6dd2c10a73ff3357bc30c988cfac6a250a1
Miyo Lee <iimiyo@naver.com>
2020-04-29 15:26:47 +0900

    커뮤니티 게시글에 카테고리 연결

--- anotherClass/firstApp/forms.py
+++ anotherClass/firstApp/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
 
-        fields = ['title', 'text']
+        fields = ['category', 'title', 'text']
 
         widgets = {
                     'title': forms.TextInput(
