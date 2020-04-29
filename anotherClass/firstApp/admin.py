bb807ac55a99c5dcc43d5753bafa1aabf71f0af8
홍지영 <0___0as@naver.com>
2020-04-29 15:55:24 +0900

    admin

--- anotherClass/firstApp/admin.py
+++ anotherClass/firstApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
 
 from django.contrib import admin
@@ -17,6 +17,10 @@ from .models import Mode
 from .models import ClassReview
 from .models import ClassQna
 
+class UserAdmin(admin.ModelAdmin) :
+    list_display = ('username', 'password')
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Category)
