21b8497f8e17cc07e614edbd6aeab9c48d1cc409
홍지영 <0___0as@naver.com>
2020-09-28 18:51:32 +0900

    메인에 클래스슬라이드

--- anotherClass/firstApp/static/js/product.js
+++ anotherClass/firstApp/static/js/product.js
@@ -3,5 +3,18 @@ $(function(){
     dots:true,
     autoplay:true,
     autoplaySpeed: 3000,
+    variableWidth: true,
+    centerMode: true,
+    centerPadding: '0px',
+    });
+});
+
+$(function(){
+    $('.autoplay').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    centerMode: true,
     });
 });
