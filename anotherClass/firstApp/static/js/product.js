75536fd1c7d131e4abb47b18a961b20e7a6f86f6
홍지영 <0___0as@naver.com>
2020-11-13 14:51:55 +0900

    모바일 수정

--- anotherClass/firstApp/static/js/product.js
+++ anotherClass/firstApp/static/js/product.js
@@ -12,7 +12,7 @@ $(function(){
 $(function(){
     $('.autoplay').slick({
     slidesToScroll: 1,
-    slidesToShow: 4,
+    slidesToShow: 3,
     autoplay: true,
     autoplaySpeed: 4000,
     infinite: false,
@@ -23,52 +23,22 @@ $(function(){
 
 $(window).resize(function() {
   if ($(window).width() < 450) {
-    $('.autoplay').slick('slickSetOption', 'slidesToShow', 1);
-    $('.autoplay').css("width", '275px');
-    $('.product-single').css("width", '80vw');
-    $('.product-single').css("height", '60vw');
-    $('.items').css("width", '80vw');
-    $('.items img').css("width", '80vw');
-    $('.items img').css("height", 'auto');
-    $('.more-container img').css("width", '80vw');
-    $('.more-container img').css("height", 'auto');
-    $('.like').removeClass("like_block");
-    $('.like').addClass("like_block_mb");
-    $('.product-info').removeClass("product-info-o");
-    $('.product-info').addClass("product-info-mb");
+    $('.autoplay').slick('slickSetOption', 'slidesToShow', 2);
+
   }
   else{
-    $('.autoplay').slick('slickSetOption', 'slidesToShow', 4);
-    $('.autoplay').css("width", '');
-    $('.like').addClass("like_block");
-    $('.like').removeClass("like_block_mb");
-    $('.product-info').addClass("product-info-o");
-    $('.product-info').removeClass("product-info-mb");
+    $('.autoplay').slick('slickSetOption', 'slidesToShow', 3);
+
   }
 });
 
 $(document).ready(function() {
   if ($(window).width() < 450) {
-    $('.autoplay').slick('slickSetOption', 'slidesToShow', 1);
-    $('.autoplay').css("width", '275px');
-    $('.product-single').css("width", '80vw');
-    $('.product-single').css("height", '60vw');
-    $('.items').css("width", '80vw');
-    $('.items img').css("width", '80vw');
-    $('.items img').css("height", 'auto');
-    $('.more-container img').css("width", '80vw');
-    $('.more-container img').css("height", 'auto');
-    $('.like').removeClass("like_block");
-    $('.like').addClass("like_block_mb");
-    $('.product-info').removeClass("product-info-o");
-    $('.product-info').addClass("product-info-mb");
+    $('.autoplay').slick('slickSetOption', 'slidesToShow', 2);
+
   }
   else{
-    $('.autoplay').slick('slickSetOption', 'slidesToShow', 4);
-    $('.autoplay').css("width", '');
-    $('.like').addClass("like_block");
-    $('.like').removeClass("like_block_mb");
-    $('.product-info').addClass("product-info-o");
-    $('.product-info').removeClass("product-info-mb");
+    $('.autoplay').slick('slickSetOption', 'slidesToShow', 3);
+
   }
 });
