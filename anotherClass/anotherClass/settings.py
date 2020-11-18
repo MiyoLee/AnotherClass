70b861cb12683e184e99a83c1cbdeaa9ee6b388f
홍지영 <0___0as@naver.com>
2020-11-19 01:46:40 +0900

    first commit

--- anotherClass/anotherClass/settings.py
+++ anotherClass/anotherClass/settings.py
@@ -31,10 +31,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3bc5m68co*^ck=iha*3cy1s#9k3970)f1v1c*kj7_0&&i5-bt@'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3bc5m68co*^ck=iha*3cy1s#9k3970)f1v1c*kj7_0&&i5-bt@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -145,3 +146,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/main'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
